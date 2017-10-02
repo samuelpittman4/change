f8262c15b69b4092f394fc4af6701027466ac49c
Samuel Pittman <samuelpittman@Samuels-MacBook-Air.local>
2017-10-02 19:29:52 -0400

    second commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int x;
+        double y, z;
+
+        x = 5;
+        y = 9.5;
+
+        z = x * y;
+
+        System.out.println("The product is " + z);
+    }
+
+}
+
